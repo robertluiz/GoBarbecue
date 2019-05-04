f844d06a096a7fc534713bb3c6257c69b854a226
Robert Luiz <robert.tiweb@gmail.com>
2019-05-03 21:41:10 -0300

    Sessão closes #12

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -16,7 +16,7 @@ class SessionController {
       console.log('Senha Incorreta')
       return res.redirect('/')
     }
-
+    req.session.user = user
     return res.redirect('/app/dashboard')
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,9 @@ routes.get('/', SessionController.create)
 routes.post('/signin', SessionController.store)
 routes.get('/signup', UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
-routes.get('/app/dashboard', (req, res) => res.render('dashboard'))
+routes.get('/app/dashboard', (req, res) => {
+  console.log(req.session.user)
+  return res.render('dashboard')
+})
 
 module.exports = routes
